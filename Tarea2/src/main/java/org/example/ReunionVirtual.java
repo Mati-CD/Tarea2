a4fb119f796eba5a6ac94f0395e386ad12993de5
Mati-CD <mcuello2024@udec.cl>
2025-05-23 19:37:26 -0400

    Actualización ReunionVirtual

--- Tarea2/src/main/java/org/example/ReunionVirtual.java
+++ Tarea2/src/main/java/org/example/ReunionVirtual.java
@@ -1,7 +1,38 @@
 package org.example;
 
-public class ReunionVirtual extends Reunion{
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class ReunionVirtual extends Reunion {
     private String enlace;
 
-    public ReunionVirtual(){super();}
+    public ReunionVirtual(Empleado organizador,
+                          TipoReunion tipo,
+                          Date fecha,
+                          LocalDateTime horaPrevista,
+                          Duration duracionPrevista,
+                          String enlace) {
+        super(organizador, tipo, fecha, horaPrevista, duracionPrevista);
+        this.enlace = enlace;
+    }
+
+    @Override
+    public String getLugar() {
+        return "Enlace virtual: " + enlace;
+    }
+
+    // Getter y Setter
+    public String getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(String enlace) {
+        this.enlace = enlace;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Modalidad: Virtual";
+    }
 }
