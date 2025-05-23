d7e8847d26086e2d0627fcfbc1cf1e28ed2974ae
Mati-CD <mcuello2024@udec.cl>
2025-05-22 21:23:52 -0400

    Getters, Setters y metodo toString en clase Empleado

--- Tarea2/src/main/java/org/example/Empleado.java
+++ Tarea2/src/main/java/org/example/Empleado.java
@@ -13,6 +13,57 @@ public class Empleado {
         this.correo = correo;
     }
 
-    // Getters y Setters
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id='" + id + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", correo='" + correo + '\'' +
+                ", departamento=" + (departamento != null ? departamento.getNombre() : "null") +
+                '}';
+    }
 
 }
