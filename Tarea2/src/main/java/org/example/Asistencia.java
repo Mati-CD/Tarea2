059d5740e85202984fcc358bbff6ec3afc8450c6
joaquinnnng <joaquinreyes246@gmail.com>
2025-05-23 06:47:47 -0400

    Agregue la clase de Asistencia que usaremos para manejar la asistencia de los empleados en la clase Reunion

---
+++ Tarea2/src/main/java/org/example/Asistencia.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.time.LocalTime;
+
+public class Asistencia {
+    private Empleado empleado;
+    private boolean presente;
+    private boolean tarde;
+    private LocalTime llegada;
+
+    public Asistencia(Empleado empleado, LocalTime horallegada , LocalTime horaInicioReunion){
+        this.empleado = empleado;
+        presente = true;
+        llegada = horallegada;
+
+        if(horallegada.isAfter(horaInicioReunion)){
+            tarde=true;
+        }else{
+            tarde=false;
+        }
+    }
+}
