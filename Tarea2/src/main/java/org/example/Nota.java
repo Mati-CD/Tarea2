00a456a4c3c43a0c6884910c4b0a13b13edeb6c2
Mati-CD <mcuello2024@udec.cl>
2025-05-23 20:14:09 -0400

    Update Nota

--- Tarea2/src/main/java/org/example/Nota.java
+++ Tarea2/src/main/java/org/example/Nota.java
@@ -5,21 +5,23 @@ import java.time.LocalDateTime;
 public class Nota {
     private String contenido;
     private LocalDateTime fechaCreacion;
-
+    
     public Nota(String contenido) {
+        if (contenido == null) {
+            throw new NullPointerException("El contenido no puede ser null");
+        }
         this.contenido = contenido;
         this.fechaCreacion = LocalDateTime.now(); // Hora local
     }
-
-    // Getters
+    
     public String getContenido() {
         return contenido;
     }
-
+    
     public LocalDateTime getFechaCreacion() {
         return fechaCreacion;
     }
-
+    
     @Override
     public String toString() {
         return "[" + fechaCreacion + "] " + contenido;
