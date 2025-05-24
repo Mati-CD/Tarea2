ab5b20ba9bec721bb8c10eec54b272610fff3b70
marquinhos03 <mliguempibozzano@gmail.com>
2025-05-23 21:27:36 -0400

    Prueba de main

--- Tarea2/src/main/java/org/example/Main.java
+++ Tarea2/src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         Nota nota = new Nota("Prueba manual");
@@ -13,7 +17,39 @@ public class Main {
         } else {
             System.out.println("✗ Error en contenido");
         }
+
+        Empleado e1 = new Empleado("001", "Cerulo", "Pierluigi", "pcerulo2023@gmail.com");
+        Empleado e2 = new Empleado("002", "Muñoz", "Carlos", "cmuñoz@gmail.com");
+        Empleado e3 = new Empleado("003", "Vidal", "Javier", "jvidal@gmail.com");
+        Empleado organizador = new Empleado("1", "Nova", "Miguel", "mnova@gmail.com");
+
+        ReunionPresencial reunion = new ReunionPresencial(
+                new Date(), organizador, tipoReunion.TECNICA
+        );
+
+        reunion.registrarInvitados(e1);
+        reunion.registrarInvitados(e2);
+        reunion.registrarInvitados(e3);
+
+        reunion.iniciar();
+
+        LocalTime horaInicio = LocalTime.of(10, 0);
+
+        reunion.registrarInvitados(e1);
+        reunion.registrarInvitados(e2);
+        reunion.registrarInvitados(e3);
+
+        reunion.registrarAsistencia(e1, LocalTime.of(9,55), horaInicio);
+        reunion.registrarAsistencia(e2, LocalTime.of(10,5), horaInicio);
+
+
+        System.out.println("LLego a la hora: " + reunion.obtenerAsistencias());
+        System.out.println("LLego tarde: " + reunion.obtenerRetrasos());
+        System.out.println("No llego: " + reunion.obtenerAusencias());
+
+
+
+
     }
 
-    // Probando clase Nota
 }
